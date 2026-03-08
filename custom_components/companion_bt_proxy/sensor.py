ec6e4f0b30302d0659a77d82e8144f8b3ffc3dff
Cronje van Heerden <cronjevanheerden@gmail.com>
2026-03-08 21:20:30 +0200

    Try again

--- custom_components/companion_bt_proxy/sensor.py
+++ custom_components/companion_bt_proxy/sensor.py
@@ -51,7 +51,7 @@ class _WatchRssiSensor(sensor.SensorEntity):
     def __init__(self, scanner, entry):
         self._attr_unique_id = "monica_watch_rssi"
         self._attr_name = "Monica Watch RSSI"
-        self._attr_native_unit_of_measurement = UnitOfPower.DECIBELS_MILLIWATT
+        self._attr_native_unit_of_measurement = UnitOfPower.DBM
         self._attr_device_class = None
         self._attr_state_class = SensorStateClass.MEASUREMENT
         self._attr_available = False
@@ -87,7 +87,7 @@ class _PhoneRssiSensor(sensor.SensorEntity):
     def __init__(self, scanner, entry):
         self._attr_unique_id = "monica_phone_rssi"
         self._attr_name = "Monica Phone RSSI"
-        self._attr_native_unit_of_measurement = UnitOfPower.DECIBELS_MILLIWATT
+        self._attr_native_unit_of_measurement = UnitOfPower.DBM
         self._attr_device_class = None
         self._attr_state_class = SensorStateClass.MEASUREMENT
         self._attr_available = False
